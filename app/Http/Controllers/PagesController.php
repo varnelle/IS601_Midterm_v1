171c821d4cd28e433ee20d54e2a51eb7d21fee18
varnelle <vpm3@njit.edu>
2018-03-27 00:10:57 -0400

    7.  Create a controller, route, and view to show a contact form and add it to the main menu of your site.

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -13,8 +13,22 @@ class PagesController extends Controller
     }
     function contact() {
         return view('pages.contact');
+
+        function store(Request $request) {
+
+            $name = $request->name;
+
+        }
+        function store(Request $request) {
+
+            $name = $request->name;
+
+            return redirect()->route('thanks',['name' => $name ]);
+        }
+        function thanks($name, Request $request) {
+
+        }
+
+            return view('pages.thankyou')->with(compact('name'));
+        }
     }
-
-
-
-}
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/about', 'PagesController@about')->name('about');
 Route::get('/contact', 'PagesController@contact')->name('contact');
+Route::post('/contact', 'PagesController@store')->name('contact.store');
