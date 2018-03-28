4ef710eb21be9518e7bbcf929ca0179589be3d42
varnelle <vpm3@njit.edu>
2018-03-28 13:34:45 -0400

    Create a unit test to insert a user Steve Smith into the users table.

--- tests/Unit/TestUsers.php
+++ tests/Unit/TestUsers.php
@@ -22,6 +22,22 @@ class TestUsers extends TestCase
         $users->email = 'my email';
         $users->password = 'secret';
         $this->assertTrue($users->save());
+
+
+
+        }
+
+
+
+        public function testUpdate()
+    {
+        $users = new User();
+        $users->name = 'Steve Smith';
+        $users->email = 'my email';
+        $users->password = 'secret';
+        $this->assertTrue($users->update());
+
+
     }
 
 }
