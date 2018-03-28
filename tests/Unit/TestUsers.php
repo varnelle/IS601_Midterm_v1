b97498a714d7071763e121d476239ea5932719ba
varnelle <vpm3@njit.edu>
2018-03-28 12:03:57 -0400

    Create a unit test to insert a user into the users table.

---
+++ tests/Unit/TestUsers.php
@@ -0,0 +1,27 @@
+<?php
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\Constraint\IsType;
+use App\User;
+
+class TestUsers extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+
+
+    public function testExample()
+    {
+        $users = new User();
+        $users->name = 'my name';
+        $users->email = 'my email';
+        $users->password = 'secret';
+        $this->assertTrue($users->save());
+    }
+
+}
