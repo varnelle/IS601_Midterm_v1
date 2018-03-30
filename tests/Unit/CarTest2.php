58e6c54c8b3ffabd68da055e35044ed14ad6fb78
varnelle <vpm3@njit.edu>
2018-03-29 23:16:27 -0400

    Including commit to check that the model is a string

---
+++ tests/Unit/CarTest2.php
@@ -0,0 +1,89 @@
+<?php
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\Constraint\IsType;
+use App\car;
+
+class CarTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+
+    public function testExample()
+    {
+        $car = new Car();
+        $car->make = 'mam llc';
+        $car->model = 'honda';
+        $car->year = '1964';
+        $this->assertTrue($car->save());
+    }
+
+    public function testUpdate()
+    {
+        $car = new Car();
+        $car->make = 'mam llc';
+        $car->model = 'honda';
+        $car->year = '2000';
+        $this->assertTrue($car->save());
+
+
+    }
+
+
+    public function testDelete()
+    {
+        $car = new Car();
+        $car->make = 'my car';
+        $car->model = 'my model';
+        $car->year = '1946';
+        $car->save();
+        $this->assertTrue($car->delete());
+
+
+    }
+
+    public function testCollectionCount()
+    {
+        $car = Car::All();
+        $recordCount = $car->count();
+        $this->assertInternalType(IsType::TYPE_INT, $recordCount);
+
+    }
+
+    public function testDataType()
+
+    {
+        //Declaring Variable
+        $year = 2000;
+        $this->assertTrue(is_int($year));
+
+    }
+
+    public function testDataType2()
+
+    {
+        //Declaring Variable
+        $model = 'my model';
+        $this->assertTrue(is_string($model));
+
+    }
+
+    public function testCar1()
+    {
+
+
+        $make = 'honda';
+        $this->assertTrue(is_string($make));
+        $make = 'toyota';
+        $this->assertTrue(is_string($make));
+        $make = 'ford';
+        $this->assertTrue(is_string($make));
+    }
+
+}
+
